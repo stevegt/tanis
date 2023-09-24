bee63fd296f9a4f0bd1696d0317c667d61572a29
Steve Traugott <stevegt@t7a.org>
2023-09-24 11:16:47 -0700

    node experiment works!
    summary of diff --git  a/x/node/node.go b/x/node/node.go
    
    - Change package name from 'main' to 'node'
    - Update comments for better explanation of 'Topic' structure
    - Change 'Topic' structure subtype from '[]float64' to 'float64' for both 'Subscribers' and 'Publish'
    - Modify functionality of 'Aggregator' structure
    - Renames 'InputTopics' and 'OutputTopic' to 'Input' and 'Subscribers' respectively in 'Aggregator'
    - Modify publishing mechanism to notify all subscribers instead of 'OutputTopic'
    - Add 'Subscribe' function to 'Aggregator'
    - Updates 'Node' structure changing 'InputTopic' and 'OutputTopic' to 'Input' and 'Output' respectively
    - Refactors input and output mechanism in 'NewNode' function
    - Remove main function and place it somewhere else
    - Remove large portion of content, specifically related to nodes, aggregators and topics creation
    - Remove logging functionality and logger setup for HTTP server
    
    summary of diff --git  a/x/node/node_test.go b/x/node/node_test.go
    
    - Add new file `node_test.go` in `x/node` directory
    - Define a `TestSimple` function for testing node operations
    - Implement test logic including creating multiple topics, aggregators, and nodes
    - Assert various test conditions such as node IDs, aggregator inputs, and node outputs
    - Test the results from the node's output channel and ensure that they match expected values
    - Ensure lengths of topics, aggregators, and nodes are as expected
    - Simulate the node results and confirm they are as expected
    - Implement debug server at `localhost:6060`

---
+++ x/node/node_test.go
@@ -0,0 +1,117 @@
+package node
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/stevegt/goadapt"
+
+	_ "net/http/pprof"
+)
+
+func TestSimple(t *testing.T) {
+	logger = NewLog()
+
+	go func() {
+		Pl(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	nodeCount := 4
+
+	// create topics, one for each node
+	topics := make([]*Topic, nodeCount)
+	for i := 0; i < nodeCount; i++ {
+		topics[i] = NewTopic()
+	}
+
+	// create aggregators -- this is the graph edge structure
+	aggregators := make([]*Aggregator, nodeCount)
+	for i := 2; i < nodeCount; i++ {
+		// connect each aggregator's inputs to the previous two topics
+		ag := NewAggregator([]*Topic{topics[i-2], topics[i-1]})
+		aggregators[i] = ag
+	}
+
+	// create nodes
+	nodes := make([]*Node, nodeCount)
+
+	// create input nodes
+	fn0 := Function{Fn: func(args ...float64) float64 { return 1.0 }, ArgCount: 0}
+	fn1 := Function{Fn: func(args ...float64) float64 { return 2.0 }, ArgCount: 0}
+	node0 := NewNode(0, fn0, nil, topics[0])
+	node1 := NewNode(1, fn1, nil, topics[1])
+	nodes[0] = node0
+	nodes[1] = node1
+
+	// create a function
+	fn := Function{
+		Fn: func(args ...float64) float64 {
+			return args[0] + args[1]
+		},
+		ArgCount: 2,
+	}
+
+	// create more nodes
+	for i := 2; i < nodeCount; i++ {
+		node := NewNode(uint64(i), fn, aggregators[i], topics[i])
+		nodes[i] = node
+	}
+
+	// subscribe to the last node's output
+	resultChan := nodes[nodeCount-1].Output.Subscribe()
+
+	// inspect the node graph
+	for i := 0; i < nodeCount; i++ {
+		Assert(nodes[i] != nil, "expected node %d to be non-nil", i)
+		Assert(nodes[i].Id == uint64(i), "expected node %d to have id %d, got %d", i, i, nodes[i].Id)
+		Pf("node %d: ", i)
+		ag := nodes[i].Input
+		Pf("input aggregator: %p ", ag)
+		if i < 2 {
+			Assert(ag == nil, "expected node %d to have nil input aggregator", i)
+		} else {
+			Assert(ag != nil, "expected node %d to have non-nil input aggregator", i)
+			Pf("aggregator inputs: ")
+			for j, input := range ag.Input {
+				Pf("%p ", input)
+				Assert(input == topics[i-2+j], "expected aggregator %d input %d to be topic %d, got %d", i, j, i-2+j, input)
+			}
+		}
+		Pf("node output: %p ", nodes[i].Output)
+		Assert(nodes[i].Output == topics[i], "expected node %d output to be topic %d, got %d", i, i, nodes[i].Output)
+		Pl()
+	}
+
+	Assert(len(topics) == nodeCount, "expected %d topics, got %d", nodeCount, len(topics))
+	Assert(len(aggregators) == nodeCount, "expected %d aggregators, got %d", nodeCount, len(aggregators))
+	Assert(len(nodes) == nodeCount, "expected %d nodes, got %d", nodeCount, len(nodes))
+
+	// read results from the result channel
+	var result float64
+	for result = range resultChan {
+		Pl(result)
+		// close the first node output topic -- this will cause
+		// everything to shut down and the result channel to close
+		// close(nodes[0].Output.Publish)
+		// XXX doesn't work yet -- we need ot get rid of node0 and node1
+		// and just publish values directly to topics[0] and
+		// topics[1], so let's just break for now
+		break
+	}
+
+	// simulate the node results
+	expecteds := make([]float64, nodeCount)
+	for i := 0; i < nodeCount; i++ {
+		switch i {
+		case 0:
+			expecteds[i] = 1.0
+		case 1:
+			expecteds[i] = 2.0
+		default:
+			expecteds[i] = expecteds[i-1] + expecteds[i-2]
+		}
+	}
+	expected := expecteds[nodeCount-1]
+	Pf("expected result: %f got %f\n", expected, result)
+	Assert(result == expected, "expected result %f, got %f", expected, result)
+}
