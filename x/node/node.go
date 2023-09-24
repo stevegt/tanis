40000b113913f141f48a425f29ee109f7064cfdf
Steve Traugott <stevegt@t7a.org>
2023-09-23 23:16:47 -0700

    try topics and aggregators
    - still deadlocks
    
    summary of diff --git  a/x/node/node.go b/x/node/node.go
    
    - Replace older dataflow implementation with pub/sub model
    - Add new 'Topic' type to encapsulate pub/sub publishing and subscribing logic
    - Replace 'NewNode' method to use new topic based model instead of previous channels model
    - Replace the 'Run' method with a new subscribe model logic incorporated within the 'NewNode' method.
    - Add new type 'Aggregator' that represents aggregation functionality
    - Introduce new Aggregator within 'NewNode' function to improve data handling
    - Implement additional error handling in 'Aggregator'
    - Modify 'Function' struct to handle float64 type arguments
    - Modify 'Node' struct to use Topic type
    - Modify main function to create new topics, aggregators and nodes, incorporate the updated functions and types, and print the results of computation.
    - Remove redundant code and comments
    - Add comments for newly introduced structures and methods for clarity
    - Update implementation to use value slices instead of individual float64 values.

--- x/node/node.go
+++ x/node/node.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"sync"
-	"time"
 
 	. "github.com/stevegt/goadapt"
 
@@ -11,120 +10,185 @@ import (
 	"net/http"
 )
 
-type Function struct {
-	Fn       func(...float64) float64
-	ArgCount int
+// Topic is a pub/sub topic.
+type Topic struct {
+	// Subscribers is a slice of channels which will receive values
+	// published to the topic.
+	Subscribers []chan []float64
+	// Publish is a channel which will receive Values to be published
+	// to the topic.
+	Publish chan []float64
 }
 
-type Node struct {
-	Id          uint64
-	Function    Function
-	InputChans  []chan float64
-	Subscribe   chan chan float64
-	Subscribers []chan float64
-}
-
-// NewNode creates a new node with the given Function.  It returns a
-// slice of input channels and a single Subscribe channel.  Reading
-// from the Subscribe channel will return a channel which will receive
-// each output of the Function.  An output is generated each time all
-// input channels have received a value and the Function has
-// calculated the result.  The number of channels in the input slice
-// is equal to the number of arguments expected by the Function.
-func NewNode(id uint64, fn Function) (n *Node) {
-	n = &Node{
-		Id:        id,
-		Function:  fn,
-		Subscribe: make(chan chan float64),
+// NewTopic creates a new Topic with the given name.
+func NewTopic() (t *Topic) {
+	t = &Topic{
+		Subscribers: make([]chan []float64, 0),
+		Publish:     make(chan []float64, 10),
 	}
-
-	// generate output channel for each subscriber
 	go func() {
+		defer func() {
+			// notify subscribers on exit
+			for _, subscriber := range t.Subscribers {
+				close(subscriber)
+			}
+		}()
+
 		for {
-			// create a new output channel
-			output := make(chan float64, 10)
-			// send to the next subscriber
-			I("node %d waiting for subscriber... ", n.Id)
-			n.Subscribe <- output
-			I("node %d sent output channel to subscriber\n", n.Id)
-			// add to the list of outputs
-			n.Subscribers = append(n.Subscribers, output)
+			// wait for a value to be published
+			value, ok := <-t.Publish
+			if !ok {
+				// channel is closed
+				return
+			}
+			// send the value to all subscribers
+			for _, subscriber := range t.Subscribers {
+				subscriber <- value
+			}
 		}
 	}()
 	return
 }
 
-// Run causes the node to subscribe to the given nodes, then starts
-// the calculation goroutine. Before starting the goroutine, each node
-// in the given slice is connected to the corresponding input channel
-// of this node.  The number of nodes in the slice must be equal to
-// the number of arguments expected by the node's function.
-func (n *Node) Run(topics []*Node) {
-	Assert(len(topics) == n.Function.ArgCount, "wrong number of topics: %d, argcount %d", len(topics), n.Function.ArgCount)
-	// for each upstream node...
-	I("node %d subscribing to %d upstream nodes\n", n.Id, len(topics))
-	for i := range topics {
-		// subscribe to the node's output
-		I("node %d subscribing to node %d... ", n.Id, topics[i].Id)
-		outChan := <-topics[i].Subscribe
-		I("node %d received output channel from node %d\n", n.Id, topics[i].Id)
-		// add to our input channels
-		n.InputChans = append(n.InputChans, outChan)
+// Subscribe returns a channel which will receive values published to
+// the topic.
+func (t *Topic) Subscribe() (c chan []float64) {
+	c = make(chan []float64, 10)
+	t.Subscribers = append(t.Subscribers, c)
+	return
+}
+
+// Aggregator takes several input topics and publishes a single output
+// topic.  The output topic is published each time all input topics
+// have received a value.
+type Aggregator struct {
+	InputTopics []*Topic
+	OutputTopic *Topic
+}
+
+// NewAggregator creates a new Aggregator with the given input and
+// output topics.
+func NewAggregator(inputTopics []*Topic, outputTopic *Topic) (a *Aggregator) {
+	a = &Aggregator{
+		InputTopics: inputTopics,
+		OutputTopic: outputTopic,
+	}
+
+	// subscribe to the input topics
+	var inputChans []chan []float64
+	for _, inputTopic := range a.InputTopics {
+		inputChans = append(inputChans, inputTopic.Subscribe())
 	}
 
 	go func() {
 		defer func() {
-			// notify subscribers on exit
-			for _, subscriber := range n.Subscribers {
-				close(subscriber)
-			}
+			// notify output topic on exit
+			close(a.OutputTopic.Publish)
 		}()
 
 		// read from all input channels until they are closed
 		closedCount := 0
 		for {
 			// read from all input channels. we need to read from each
-			// channel in a separate goroutine to avoid blocking and
-			// handle the case where one or more channels are closed.
-			inputs := make([]float64, len(n.InputChans))
+			// in a separate goroutine to avoid blocking and handle
+			// the case where one or more channels are closed.
+			values := make([]float64, len(inputChans))
 			wg := &sync.WaitGroup{}
-			for i := range n.InputChans {
+			for i := range inputChans {
 				wg.Add(1)
-				// start goroutine for channel i
+				// start goroutine for topic i
 				go func(i int) {
 					defer wg.Done()
-					// read one value from channel i
-					I("node %d waiting for input on channel %d... ", n.Id, i)
-					input, ok := <-n.InputChans[i]
+					// read one value from topic i
+					input, ok := <-inputChans[i]
 					if ok {
-						I("node %d received input on channel %d\n", n.Id, i)
 						// channel is open
-						inputs[i] = input
-						Pl("node", n.Id, "received", input, "on input", i)
+						// XXX only using the first value in the input
+						// slice
+						values[i] = input[0]
 						return
 					}
 					// channel is closed
 					closedCount++
-					Pl("node", n.Id, "input", i, "closed")
 				}(i)
 			}
 			// wait for all input channels to be read
 			wg.Wait()
-			if closedCount == len(n.InputChans) {
-				Pl("node", n.Id, "all inputs closed")
+			if closedCount == len(inputChans) {
 				return
+			}
+
+			// send the values slice to the output topic
+			a.OutputTopic.Publish <- values
+
+		}
+	}()
+	return
+}
+
+// Function is a function which takes a slice of float64 arguments and
+// returns a float64 result.  The number of arguments is specified by
+// ArgCount.
+type Function struct {
+	Fn       func(...float64) float64
+	ArgCount int
+}
+
+// Node is a node in a dataflow graph.  It has a Function which
+// calculates a result from a slice of input values.
+type Node struct {
+	Id          uint64
+	Function    Function
+	InputTopic  *Topic
+	OutputTopic *Topic
+}
+
+// NewNode creates a new node with the given Function.
+// An output is generated each time a value slice is received on the
+// input topic.
+func NewNode(id uint64, fn Function, inputTopic *Topic, outputTopic *Topic) (n *Node) {
+
+	n = &Node{
+		Id:          id,
+		Function:    fn,
+		InputTopic:  inputTopic,
+		OutputTopic: outputTopic,
+	}
+
+	var inputChan chan []float64
+	if n.InputTopic != nil {
+		// subscribe to the input topic
+		c := n.InputTopic.Subscribe()
+		inputChan = c
+	}
+
+	outputChan := n.OutputTopic.Publish
+
+	go func() {
+		defer func() {
+			// notify output topic on exit
+			close(outputChan)
+		}()
+
+		for {
+			var inputs []float64
+			if inputChan != nil {
+				// read a value slice from the input topic
+				in, ok := <-inputChan
+				inputs = in
+				if !ok {
+					// channel is closed
+					return
+				}
 			}
 
 			// calculate the result
 			result := n.Function.Fn(inputs...)
-			// send the result to all subscribers
-			for _, subscriber := range n.Subscribers {
-				I("node %d sending result to subscriber... ", n.Id)
-				subscriber <- result
-				I("node %d sent result to subscriber\n", n.Id)
-			}
+			// send the result to the output topic
+			outputChan <- []float64{result}
 		}
 	}()
+	return
 }
 
 func main() {
@@ -134,6 +198,37 @@ func main() {
 		Pl(http.ListenAndServe("localhost:6060", nil))
 	}()
 
+	nodeCount := 4
+
+	// create topics, one for each node
+	var topics []*Topic
+	for i := 0; i < nodeCount; i++ {
+		topics = append(topics, NewTopic())
+	}
+
+	// create aggregators
+	var aggregators []*Aggregator
+	for i := 2; i < nodeCount; i++ {
+		// connect each aggregator's inputs to the previous two
+		// topics, and its output to the current topic
+		ag := NewAggregator([]*Topic{topics[i-2], topics[i-1]}, topics[i])
+		aggregators = append(aggregators, ag)
+	}
+
+	// subscribe to the last aggregator's output
+	resultChan := aggregators[len(aggregators)-1].OutputTopic.Subscribe()
+
+	// create nodes
+	var nodes []*Node
+
+	// create input nodes
+	fn0 := Function{Fn: func(args ...float64) float64 { return 1.0 }, ArgCount: 0}
+	fn1 := Function{Fn: func(args ...float64) float64 { return 2.0 }, ArgCount: 0}
+	node0 := NewNode(0, fn0, nil, topics[0])
+	node1 := NewNode(1, fn1, nil, topics[1])
+	nodes = append(nodes, node0)
+	nodes = append(nodes, node1)
+
 	// create a function
 	fn := Function{
 		Fn: func(args ...float64) float64 {
@@ -142,45 +237,17 @@ func main() {
 		ArgCount: 2,
 	}
 
-	var nodes []*Node
-
-	// create input nodes
-	input1 := NewNode(0, Function{Fn: func(args ...float64) float64 { return 1.0 }, ArgCount: 0})
-	input2 := NewNode(1, Function{Fn: func(args ...float64) float64 { return 2.0 }, ArgCount: 0})
-	nodes = append(nodes, input1)
-	nodes = append(nodes, input2)
-
 	// create more nodes
 	for i := 0; i < 2; i++ {
-		nodes = append(nodes, NewNode(uint64(len(nodes)), fn))
+		node := NewNode(uint64(len(nodes)), fn, aggregators[i].OutputTopic, topics[i])
+		nodes = append(nodes, node)
 	}
 
-	// run the nodes
-	input1.Run(nil)
-	input2.Run(nil)
-	for i := 2; i < len(nodes); i++ {
-		// connect each node's inputs to the previous two nodes' outputs
-		me := nodes[i]
-		upstream1 := nodes[i-2]
-		upstream2 := nodes[i-1]
-		Pl("connecting node", me.Id, "to upstream nodes", upstream1.Id, upstream2.Id)
-		nodes[i].Run([]*Node{upstream1, upstream2})
-	}
+	Assert(len(nodes) == nodeCount, "expected %d nodes, got %d", nodeCount, len(nodes))
 
-	lastNode := nodes[len(nodes)-1]
-	Pl("main subscribing to last node", lastNode.Id)
-	resultChan := <-lastNode.Subscribe
-
-	time.Sleep(1 * time.Second)
-
-	for _, node := range nodes {
-		println("node", node.Id, "has", len(node.Subscribers), "subscribers")
-	}
-
-	// read results from the last node
-	Pl("main reading from result channel... ")
+	// read results from the result channel
 	for result := range resultChan {
-		println(result)
+		Pl(result)
 	}
 
 }
