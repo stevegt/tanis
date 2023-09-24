fd81f743d740f85d4fbc7590e53fc39a70e18b43
Steve Traugott <stevegt@t7a.org>
2023-09-24 11:46:48 -0700

    x/node: fix shutdown, remove first two nodes
    summary of diff --git  a/x/node/node_test.go b/x/node/node_test.go
    
    - Comment out the creation of input nodes in the test
    - Adjust node creation loop to start from the 2nd index rather than 1st
    - Remove condition of checking for nodes having nil input aggregators
    - Remove a block of code that asserts the count of topics, aggregators, and nodes
    - Add code to publish values to the first two topics
    - Modify the closing of first node output topic to closing the first two topics
    - Remove outdated commented out code related to handling node output topic

--- x/node/node_test.go
+++ x/node/node_test.go
@@ -35,13 +35,15 @@ func TestSimple(t *testing.T) {
 	// create nodes
 	nodes := make([]*Node, nodeCount)
 
-	// create input nodes
-	fn0 := Function{Fn: func(args ...float64) float64 { return 1.0 }, ArgCount: 0}
-	fn1 := Function{Fn: func(args ...float64) float64 { return 2.0 }, ArgCount: 0}
-	node0 := NewNode(0, fn0, nil, topics[0])
-	node1 := NewNode(1, fn1, nil, topics[1])
-	nodes[0] = node0
-	nodes[1] = node1
+	/*
+		// create input nodes
+		fn0 := Function{Fn: func(args ...float64) float64 { return 1.0 }, ArgCount: 0}
+		fn1 := Function{Fn: func(args ...float64) float64 { return 2.0 }, ArgCount: 0}
+		node0 := NewNode(0, fn0, nil, topics[0])
+		node1 := NewNode(1, fn1, nil, topics[1])
+		nodes[0] = node0
+		nodes[1] = node1
+	*/
 
 	// create a function
 	fn := Function{
@@ -51,7 +53,7 @@ func TestSimple(t *testing.T) {
 		ArgCount: 2,
 	}
 
-	// create more nodes
+	// create nodes
 	for i := 2; i < nodeCount; i++ {
 		node := NewNode(uint64(i), fn, aggregators[i], topics[i])
 		nodes[i] = node
@@ -61,42 +63,35 @@ func TestSimple(t *testing.T) {
 	resultChan := nodes[nodeCount-1].Output.Subscribe()
 
 	// inspect the node graph
-	for i := 0; i < nodeCount; i++ {
+	for i := 2; i < nodeCount; i++ {
 		Assert(nodes[i] != nil, "expected node %d to be non-nil", i)
 		Assert(nodes[i].Id == uint64(i), "expected node %d to have id %d, got %d", i, i, nodes[i].Id)
 		Pf("node %d: ", i)
 		ag := nodes[i].Input
 		Pf("input aggregator: %p ", ag)
-		if i < 2 {
-			Assert(ag == nil, "expected node %d to have nil input aggregator", i)
-		} else {
-			Assert(ag != nil, "expected node %d to have non-nil input aggregator", i)
-			Pf("aggregator inputs: ")
-			for j, input := range ag.Input {
-				Pf("%p ", input)
-				Assert(input == topics[i-2+j], "expected aggregator %d input %d to be topic %d, got %d", i, j, i-2+j, input)
-			}
+		Assert(ag != nil, "expected node %d to have non-nil input aggregator", i)
+		Pf("aggregator inputs: ")
+		for j, input := range ag.Input {
+			Pf("%p ", input)
+			Assert(input == topics[i-2+j], "expected aggregator %d input %d to be topic %d, got %d", i, j, i-2+j, input)
 		}
 		Pf("node output: %p ", nodes[i].Output)
 		Assert(nodes[i].Output == topics[i], "expected node %d output to be topic %d, got %d", i, i, nodes[i].Output)
 		Pl()
 	}
 
-	Assert(len(topics) == nodeCount, "expected %d topics, got %d", nodeCount, len(topics))
-	Assert(len(aggregators) == nodeCount, "expected %d aggregators, got %d", nodeCount, len(aggregators))
-	Assert(len(nodes) == nodeCount, "expected %d nodes, got %d", nodeCount, len(nodes))
+	// publish values to the first two topics
+	topics[0].Publish <- 1.0
+	topics[1].Publish <- 2.0
 
 	// read results from the result channel
 	var result float64
 	for result = range resultChan {
 		Pl(result)
-		// close the first node output topic -- this will cause
+		// close the first two topics -- this will cause
 		// everything to shut down and the result channel to close
-		// close(nodes[0].Output.Publish)
-		// XXX doesn't work yet -- we need ot get rid of node0 and node1
-		// and just publish values directly to topics[0] and
-		// topics[1], so let's just break for now
-		break
+		close(topics[0].Publish)
+		close(topics[1].Publish)
 	}
 
 	// simulate the node results
